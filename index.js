c466f89132c47473fa56fe3595ae67ca4002051b
Oscar <oscarmondragon100@gmail.com>
2018-01-05 00:18:28 -0600

    Package, dependecias e index.js
    Se inicializo el proyecto, se instalaron las dependecias del proyecto,
    ademas se creo el archivo index.js para realizar la conexion con la base
    de datos

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strct';
 
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/spotyDB', (err,res)=> {
     if(err){
+        throw err;
     } else{
+        console.log('La base de datos esta corriedo correctamente');
     }
 });
