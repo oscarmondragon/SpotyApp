4627fbdcdb08e5528f7076931131243c43e41859
Oscar <oscarmondragon100@gmail.com>
2018-01-13 00:11:50 -0600

    Middleware de autenticacion con jwt y updateUser
    Se implemento un middleware de autenticacion del usuario con jwt y
    ademas se agrego la funcion de actualizar usuario en el controlador

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var UserController = require('../controllers/user.controller');
 var api = express.Router();
-api.get('/prueba-controlador', UserController.pruebas);
+var md_auth = require('../middlewares/authenticated');
+api.get('/prueba-controlador',md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id',md_auth.ensureAuth, UserController.updateUser);
 module.exports = api;
