a6ac6a18218395793d0e1a4eebb0c4085160e157
Oscar <oscarmondragon100@gmail.com>
2018-01-14 22:24:59 -0600

    Subir imagen al usuario
    Se creo una funcion para subir imagen de usuario con multiparty en el
    controlador del usuario y se creo una carpeta para almacenarlas.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -66,7 +66,6 @@ function loginUser(req, res) {
                 message: 'Error en la peticion'
             });
         } else {
-            console.log(user);
             if (!user) {
                 res.status(404).send({
                     message: 'El usuario no existe'
@@ -78,8 +77,9 @@ function loginUser(req, res) {
                         //devolver datos del usuario logueado
                         if (params.gethash) {
                             //devolver un toquen de jwt
-                            res.status(200).send({token: jwt.createToken(user)});
-                            
+                            res.status(200).send({
+                                token: jwt.createToken(user)
+                            });
                         } else {
                             res.status(200).send({
                                 user
@@ -95,8 +95,76 @@ function loginUser(req, res) {
         }
     });
 }
+
+function updateUser(req, res) {
+    var userId = req.params.id;
+    var update = req.body;
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+        if (err) {
+            res.status(500).send({
+                message: 'Error al actualizar el usuario'
+            });
+        } else {
+            if (!userUpdated) {
+                res.status(404).send({
+                    message: ' No se ha podido actualizar el usuario'
+                });
+            } else {
+                res.status(200).send({
+                    user: userUpdated
+                });
+            }
+        }
+    });
+}
+
+function uploadImage(req, res) {
+    var userId = req.params.id;
+    var file_name = "No subido";
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\'); // separa el path para obtener solo el nombre de la imagen
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1]; // obtiene la extencion de la imagen
+        console.log(file_split);
+        console.log(file_name);
+        console.log(file_ext);
+        if (file_ext == 'png' || file_ext == 'gif' || file_ext == 'jpg'  || file_ext == 'PNG'  || file_ext == 'JPG' || file_ext == 'GIF') {
+            User.findByIdAndUpdate(userId, {
+                image: file_name
+            }, (err, userUpdated) => {
+                if (err) {
+                    res.status(500).send({
+                        message: 'Error al actualizar el usuario'
+                    });
+                } else {
+                    if (!userUpdated) {
+                        res.status(404).send({
+                            message: ' No se ha podido actualizar el usuario'
+                        });
+                    } else {
+                        res.status(200).send({
+                            user: userUpdated
+                        });
+                    }
+                }
+            });
+        } else {
+            res.status(200).send({
+                message: ' Extencion del archivo no valida'
+            });
+        }
+    } else {
+        res.status(200).send({
+            message: ' No ha subido ninguna imagen'
+        });
+    }
+}
 module.exports = {
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    updateUser,
+    uploadImage
 };
